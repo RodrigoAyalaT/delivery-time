d082333aa5491c495d5be8b80a965868314695fd
Cristian Incarnato <cristian.cdi@gmail.com>
2021-08-24 17:13:30 -0300

    Home Back

--- apps/frontend/src/modules/base/pages/HomePage/HomePage.vue
+++ apps/frontend/src/modules/base/pages/HomePage/HomePage.vue
@@ -1,19 +1,68 @@
 <template>
-    <v-container fill-height>
-        <v-row justify="center" align="center">
-            <v-col cols="12" class="text-center">
-                <v-icon>home</v-icon>
-            </v-col>
+  <v-container fill-height>
+    <v-row justify="center" align="center">
+      <v-col cols="12" class="text-center">
+        <v-img height="160" contain src="@/assets/img/LogoTremendas7.png"></v-img>
+      </v-col>
+      <v-col cols="12" class="text-center">
+        <v-btn x-large
+               rounded
+               color="primary"
+               :to="{name:'OrderPage'}"
+        >
+          REALIZAR PEDIDO
+        </v-btn>
+        <h6 class="text-h6 grey--text text--darken-2 mt-2">Horario de 19 a 22 hs</h6>
+      </v-col>
+      <v-col cols="12" class="text-center">
+        <v-row justify="center">
+          <v-col cols="12" sm="4">
+            <v-card>
+              <v-img
+                  gradient="to top , rgba(40,40,0,0.9), rgba(235,229,120,0.4)"
+                  height="130"
+                  src="@/assets/img/Cocidas-1.jpg"
+                  class="white--text d-flex align-center"
+              >
+                <div>
+                  <h4 class="text-h4 font-weight-bold"> HORNEADAS</h4>
+                  <p class="mb-0">
+                    No queres hacer nada, solo entrarle a morir a unas empandas <b>tremendas</b>... Hacé tu pedido!
+                  </p>
+                </div>
+              </v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <v-card>
+              <v-img
+                  gradient="to top right,  rgba(6,13,82,0.9), rgba(27,50,162,0.4)"
+                  height="130"
+                  src="@/assets/img/Congeladas-2.jpg"
+                  class="white--text d-flex align-center"
+              >
+                <div>
+                  <h4 class="text-h4 font-weight-bold"> CONGELADAS</h4>
+                  <p class="mb-0">
+                    <b>Tremendas</b> llega hasta tu casa congeladas, listas para hornear en sólo unos minutos!
+                  </p>
+                </div>
+              </v-img>
+            </v-card>
+          </v-col>
+
+
         </v-row>
-    </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        name: 'HomePage',
-    }
+export default {
+  name: 'HomePage',
+}
 </script>
 
 <style scoped>
-
 </style>
